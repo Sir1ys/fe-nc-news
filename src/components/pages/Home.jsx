6748165eaf441ba52f787306ac5ac968e87e4433
Sir1ys <sashakoroviy@gmail.com>
2023-10-24 11:15:59 +0100

    loader component was added

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ArticleCard from "../ArticleCard";
+import Loader from "../Loader";
 
-export default function Home({ articles }) {
-
+export default function Home({ articles, loading }) {
   return (
     <section className="articles">
-      {articles.map((article, index) => (
-        <ArticleCard key={index} article={article}/>
-      ))}
+      {loading ? (
+        <Loader />
+      ) : (
+        articles.map((article, index) => (
+          <ArticleCard key={index} article={article} />
+        ))
+      )}
     </section>
   );
 }
